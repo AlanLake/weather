847238cc5111f403d53dba3371d840a70f0463a2
AlanLake <alan.lake@mail.ru>
2021-01-30 17:28:52 +0300

    added moment js and city timezones

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-const key = '9302a69eb83ac8da83def37be0ee66e8';
-const main = document.getElementById('main')
-const form = document.getElementById('weatherform');
-const search = document.getElementById('search');
-const currentDate = document.getElementById('date');
-
-
-
+const cityTimezones = require("city-timezones");
+var unique = require("uniq");
+const key = "9302a69eb83ac8da83def37be0ee66e8";
+const main = document.getElementById("main");
+const form = document.getElementById("weatherform");
+const search = document.getElementById("search");
+const currentDate = document.getElementById("date");
+const moment = require("moment-timezone");
 
 const url = (location, apikey) =>
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
+  `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
 
-
-
-async function getWeatherByLocation(location){
-    const resp = await fetch(url(location), {origin: 'cors'});
-    const respData = await resp.json();
-    addWeatherToPage(respData)
+async function getWeatherByLocation(location) {
+  const resp = await fetch(url(location), { origin: "cors" });
+  const respData = await resp.json();
+  addWeatherToPage(respData);
 }
 
+function addWeatherToPage(data) {
+  const temp = Math.floor(KtoC(data.main.temp));
 
-function addWeatherToPage(data){
-    const temp = Math.floor(KtoC(data.main.temp));
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
 
-    const weather = document.createElement('div');
-    weather.classList.add('weather');
-
-    weather.innerHTML = `
+  weather.innerHTML = `
     <h2><span>
     <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
     </span>${temp} °C <span>
@@ -34,77 +31,75 @@ function addWeatherToPage(data){
     <p>in ${search.value}</p>
     <p> ${data.weather[0].description} </p>
     `;
-    main.innerHTML = '';
-    changeBackground(today);
-    main.appendChild(weather);
+  main.innerHTML = "";
+
+  main.appendChild(weather);
 }
-function KtoC(K){
-    return (K - 273.15).toFixed(2);
+function KtoC(K) {
+  return (K - 273.15).toFixed(2);
 }
+var time;
+var timeForBck;
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-
-    const location = search.value;
-
-    if(location){
-        getWeatherByLocation(location);
-        addDayToPage();
-    }
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  const timezone = cityTimezones.lookupViaCity(location)[0].timezone;
+  time = moment().tz(timezone).format("HH:mm:ss");
+  timeForBck = moment().tz(timezone).format("HH");
+  if (location) {
+    getWeatherByLocation(location);
+    changeBackground(timeForBck);
+    addDayToPage(time);
+  }
+});
 
 var today = new Date();
 
-function changeBackground(today){
-    if (today.getHours() >= 22 || today.getHours() < 6) {
-        document.getElementById('body').style.background = 'black'
-        document.getElementById('body').style.color = 'white'
-    }
-    else if(today.getHours() < 22 && today.getHours() > 17){
-        document.getElementById('body').style.background = '#00008B'
-        document.getElementById('body').style.color = 'white'
-    }
-    else if (today.getHours() >= 6 || today.getHours() < 12) {
-        document.getElementById('body').style.background = '#FFD700'
-        document.getElementById('body').style.color = 'black'
-    }
-    else if (today.getHours() >= 12 || today.getHours() <= 17) {
-        document.getElementById('body').style.background = '#87CEEB'
-        document.getElementById('body').style.color = 'black'
-    }
+function changeBackground(timeForBck) {
+  console.log(timeForBck);
+  if (timeForBck >= 22 || timeForBck < 6) {
+    document.getElementById("body").style.background = "black";
+    document.getElementById("body").style.color = "white";
+  } else if (timeForBck < 22 && timeForBck > 17) {
+    document.getElementById("body").style.background = "#00008B";
+    document.getElementById("body").style.color = "white";
+  } else if (timeForBck >= 6 && timeForBck < 12) {
+    document.getElementById("body").style.background = "#FFD700";
+    document.getElementById("body").style.color = "black";
+  } else if (timeForBck >= 12 && timeForBck <= 17) {
+    document.getElementById("body").style.background = "#87CEEB";
+    document.getElementById("body").style.color = "black";
+  }
 }
 
-function currentDay(today){
-    return today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+function currentDay(today) {
+  return (
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate()
+  );
 }
-function currentTime(today){
-    if(today.getHours() >= 10 && today.getMinutes() >= 10){
-        return `${today.getHours()}:${today.getMinutes()}`
-    }
-    else if(today.getHours() < 10 && today.getMinutes() < 10){
-        return `0${today.getHours()}:0${today.getMinutes()}`
-    }
-    else if(today.getHours() >= 10 && today.getMinutes() <= 10){
-        return `${today.getHours()}:0${today.getMinutes()}`
-    }
-    else if(today.getHours() <= 10 && today.getMinutes() >= 10){
-        return `0${today.getHours()}:${today.getMinutes()}`
-    }
+function currentTime(today) {
+  if (today.getHours() >= 10 && today.getMinutes() >= 10) {
+    return `${today.getHours()}:${today.getMinutes()}`;
+  } else if (today.getHours() < 10 && today.getMinutes() < 10) {
+    return `0${today.getHours()}:0${today.getMinutes()}`;
+  } else if (today.getHours() >= 10 && today.getMinutes() <= 10) {
+    return `${today.getHours()}:0${today.getMinutes()}`;
+  } else if (today.getHours() <= 10 && today.getMinutes() >= 10) {
+    return `0${today.getHours()}:${today.getMinutes()}`;
+  }
 }
 
-function addDayToPage(){
-    const date = document.createElement('div');
-    date.classList.add('currentDate');
+function addDayToPage() {
+  const date = document.createElement("div");
+  date.classList.add("currentDate");
 
-    date.innerHTML = `
+  date.innerHTML = `
     <h2>Today is ${currentDay(today)} </h2>
-    <small>Current time is ${currentTime(today)}</small>`
-    
-    currentDate.innerHTML = ''
-    currentDate.appendChild(date)
+    <small>Current time is ${time}</small>`;
+
+  currentDate.innerHTML = "";
+  currentDate.appendChild(date);
 }
 
-currentDay(today)
-console.log(today.getDate(), today.getMonth() + 1, today.getHours(), today.getMinutes())
-
-
+currentDay(today);
